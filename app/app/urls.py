422ac8c625648528fe45bc5072a4dac8ea26dbda
jmunoz <omnibnk@OmniBnks-MacBook-Air-2.local>
2019-04-04 16:29:27 -0500

    add tags api

--- app/app/urls.py
+++ app/app/urls.py
@@ -19,5 +19,6 @@ from django.urls import path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/user/', include('user.urls'))
+    path('api/user/', include('user.urls')),
+    path('api/recipe/', include('recipe.urls')),
 ]
